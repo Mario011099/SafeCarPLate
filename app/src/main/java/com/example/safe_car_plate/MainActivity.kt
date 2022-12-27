2148d8ef213c448865da81bf357cf48420f18ed1
Mario011099 <mario.sanchez@grupoasinfo.com>
2022-12-26 22:05:29 -0500

    primer commit

---
+++ app/src/main/java/com/example/safe_car_plate/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.safe_car_plate
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.example.safe_car_plate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    //@SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(licenseplate())
+
+       binding.navBottom1.setOnItemSelectedListener {
+
+           when(it.itemId){
+                R.id.search_licenseplate -> replaceFragment(licenseplate())
+                R.id.search_crimes -> replaceFragment(crimes())
+                R.id.about -> replaceFragment(about())
+
+                else ->{
+
+                }
+            }
+           true
+        }
+    }
+
+     fun replaceFragment( fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
