ca600e05dbe88171ec1d8dec1a1b5868d4c82759
Mario011099 <mario.sanchez@grupoasinfo.com>
2023-01-11 22:48:12 -0500

    Se agrega funcion para abrir cámara

--- app/src/main/java/com/example/safe_car_plate/licenseplate.kt
+++ app/src/main/java/com/example/safe_car_plate/licenseplate.kt
@@ -1,15 +1,25 @@
 package com.example.safe_car_plate
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import com.example.safe_car_plate.databinding.FragmentLicenseplateBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
+lateinit var currentPhotoPath: String
+lateinit var binding: FragmentLicenseplateBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +27,77 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class licenseplate : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_licenseplate, container, false)
+        binding = FragmentLicenseplateBinding.inflate(inflater, container, false)
+        initClass()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment licenseplate.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            licenseplate().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            context?.let {
+                takePictureIntent.resolveActivity(it.packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile()
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        null
+                    }
+                    // Continue only if the File was successfully created
+                    photoFile?.also {
+                        val photoURI: Uri = FileProvider.getUriForFile(
+                            requireContext(), "com.example.android.fileprovider", it
+                        )
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    }
                 }
             }
+        }
     }
+
+    private fun initClass() {
+        binding.subirFoto.setOnClickListener {
+            var photoFile: File? = null
+            photoFile = createImageFile()
+            var imageUri: Uri
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            val uriSavedImage = Uri.fromFile(File("/storage/emulated/0/Pictures/image.jpg"))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                imageUri = Uri.parse("/storage/emulated/0/Pictures/image.jpg");
+            } else {
+                imageUri = Uri.fromFile(File("/storage/emulated/0/Pictures/image.jpg"));
+            }
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivity(intent)
+
+
+        }
+
+    }
+
+
 }
