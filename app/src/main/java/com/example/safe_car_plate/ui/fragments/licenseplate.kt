83164804cb17f45e1fafabdd85e45a268a2d0a9d
Angiey1 <angiey1@hotmail.com>
2023-02-04 22:04:03 -0500

    Primeros cambios con API

--- app/src/main/java/com/example/safe_car_plate/ui/fragments/licenseplate.kt
+++ app/src/main/java/com/example/safe_car_plate/ui/fragments/licenseplate.kt
@@ -1,21 +1,21 @@
 package com.example.safe_car_plate.ui.fragments
 
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.FileProvider
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.safe_car_plate.R
+import com.example.safe_car_plate.databinding.FragmentCrimesBinding
 import com.example.safe_car_plate.databinding.FragmentLicenseplateBinding
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -25,8 +25,14 @@ import java.util.*
  * create an instance of this fragment.
  */
 class licenseplate : Fragment() {
-    lateinit var currentPhotoPath: String
     lateinit var binding: FragmentLicenseplateBinding
+
+    //my
+    private lateinit var placaImageView: ImageView
+    private lateinit var runButton: ImageButton
+    private lateinit var consultaButton: Button
+    private lateinit var imageInput: InputImage
+    private lateinit var placa: String
 
 
     override fun onCreateView(
@@ -34,82 +40,137 @@ class licenseplate : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentLicenseplateBinding.inflate(inflater, container, false)
-        initClass()
         return binding.root
+
     }
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+    override fun onStart() {
+        super.onStart()
+        initClass()
+    }
+
+
+    private fun runTextRecognition() {
+        val recognizer = TextRecognition.getClient()
+        runButton.setEnabled(false)
+        recognizer.process(imageInput)
+            .addOnSuccessListener { texts ->
+                runButton.setEnabled(true)
+                processTextRecognitionResult(texts)
+            }
+            .addOnFailureListener { e -> // Task failed with an exception
+                runButton.setEnabled(true)
+                e.printStackTrace()
+            }
+    }
+
+    private fun processTextRecognitionResult(texts: Text) {
+        var wordsList: MutableList<String?> = ArrayList<String?>()
+        val blocks = texts.textBlocks
+        if (blocks.size == 0) {
+            showToast("No text found")
+            return
         }
-    }
-
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
-            context?.let {
-                takePictureIntent.resolveActivity(it.packageManager)?.also {
-                    // Create the File where the photo should go
-                    val photoFile: File? = try {
-                        createImageFile()
-                    } catch (ex: IOException) {
-                        // Error occurred while creating the File
-                        null
-                    }
-                    // Continue only if the File was successfully created
-                    photoFile?.also {
-                        val photoURI: Uri = FileProvider.getUriForFile(
-                            requireContext(), "com.example.android.fileprovider", it
-                        )
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    }
+        for (i in blocks.indices) {
+            val lines = blocks[i].lines
+            for (j in lines.indices) {
+                val elements = lines[j].elements
+                for (k in elements.indices) {
+                    println("Palabra " + elements[k].text)
+                    wordsList.add(elements[k].text)
                 }
             }
         }
+        getListWords(wordsList)
+
     }
 
-    private fun initClass() {
+    private fun getListWords(wordsList: MutableList<String?>) {
+        var v = wordsList.filter { s-> !(s.equals("ECUADOR") || s.equals("ANT")) }
+        println(v)
+        if(v.isNotEmpty()){
+            placa = v[0]?.uppercase()!!.replace("-","")
+            binding.textResultModel.text = v[0]?.uppercase()
+            binding.btnConsultar.visibility = View.VISIBLE
+            binding.idTextTitlePlaca.visibility = View.VISIBLE
+
+        }else{
+            showToast("No se encontró ninguna placa")
+        }
+
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun initClass(){
+        runButton = binding.buttonBuscar
+        consultaButton = binding.btnConsultar
         binding.subirFoto.setOnClickListener {
-            var photoFile: File? = null
-            photoFile = createImageFile()
-            var imageUri: Uri
-            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//            val uriSavedImage = Uri.fromFile(File("/storage/emulated/0/Pictures/image.jpg"))
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                imageUri = Uri.parse("/storage/emulated/0/Pictures/image.jpg");
-            } else {
-                imageUri = Uri.fromFile(File("/storage/emulated/0/Pictures/image.jpg"));
-            }
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-            startActivity(intent)
-
-
+            requestPermission()
         }
-
-        binding.buttonBuscar.setOnClickListener {
-            var placa = binding.textPlaca.text.toString()
-            val bundle = Bundle()
-            bundle.putString("placa", placa)
-            val crimesFragment = crimes()
-            crimesFragment.arguments = bundle
-            val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.frame_layout, crimesFragment)
-            transaction?.commit()
+        consultaButton.setOnClickListener {
+            consultarDatos()
         }
-
 
     }
 
+    private fun cargarImagen(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        empezarForActivityGallery.launch(intent)
+    }
+
+    private val empezarForActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val dato = result.data?.data
+            binding.imgVPlaca.setImageURI(dato)
+            placaImageView = binding.imgVPlaca
+            imageInput = InputImage.fromFilePath(activity?.baseContext!!, result.data?.data!!)
+            runTextRecognition()
+            println("Data $dato")
+        }
+
+    }
+
+    private fun requestPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(
+                    activity?.baseContext!!,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    cargarImagen()
+                }
+                else -> requestPermissionL.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            cargarImagen()
+        }
+    }
+
+    private val requestPermissionL = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isOtorgado ->
+        if (isOtorgado){
+            cargarImagen()
+        }else{
+            Toast.makeText(activity?.baseContext, "Necesita habilitar los permisos",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun consultarDatos() {
+
+        var intent = Intent(activity?.baseContext!!, FragmentCrimesBinding::class.java)
+        intent.putExtra("placa",placa)
+//        startActivity(intent)
+//        val mFragment: Fragment = FragmentCrimesBinding()
+//        fragmentManager().beginTransaction().replace(R.id.content_frame, mFragment)
+//            .commit()
+    }
 
 }
