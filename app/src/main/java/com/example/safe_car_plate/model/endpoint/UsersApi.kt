ccb2331b0c4398bb2427030a8c96ef019dd2fc79
Angiey1 <angiey1@hotmail.com>
2023-02-25 23:24:08 -0500

    Id api

--- app/src/main/java/com/example/safe_car_plate/model/endpoint/UsersApi.kt
+++ app/src/main/java/com/example/safe_car_plate/model/endpoint/UsersApi.kt
@@ -1,10 +1,16 @@
 package com.example.safe_car_plate.model.endpoint
 
+import com.example.safe_car_plate.model.entities.api.PlacaApiResult
+import com.example.safe_car_plate.model.entities.api.Users
 import com.example.safe_car_plate.model.entities.api.UsersApiResult
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface UsersApi {
     @GET("users")
     suspend fun getUsers() : Response<UsersApiResult>
+
+    @GET("users/{id}")
+    suspend fun getUserById(@Path("id") id: String) : Response<Users>
 }
